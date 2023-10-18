24af6bad3cadeb56349f6808abec0ccd8e209f3d
jorenheit <jorenheit@gmail.com>
2023-10-18 19:12:26 +0200

    lcd driver (nano)

---
+++ settings.h
@@ -0,0 +1,39 @@
+#pragma once
+
+enum LCDDriverPins {
+  SH_CP = 4, // 5
+  DS = 3,    // 4
+  ST_CP = 2  // 3  --> need pin 2 for interrupt handling
+};
+
+enum ModuleDriverPins {
+  CLOCK_INTERRUPT_PIN = 2,
+  WRITE_ENABLE_PIN = A0,  // should be able to do digitalRead on analog pin, right?
+  DISPLAY_MODE_PIN = A1,
+
+  D0 = 6,
+  D1 = 7,
+  D2 = 8,
+  D3 = 9,
+  D4 = 10,
+  D5 = 11,
+  D6 = 12,
+  D7 = 13
+};
+
+inline constexpr int DATA_PINS[] = {
+  D0, D1, D2, D3, D4, D5, D6, D7
+};
+
+enum LCDParams {
+  LINES = 2,
+  CHARS = 16,
+  TAB = 4,
+  REFRESH_DELAY = 100
+};
+
+enum DisplayMode {
+  ASCII = 0,
+  DECIMAL = 10,
+  HEXADECIMAL = 16
+};
