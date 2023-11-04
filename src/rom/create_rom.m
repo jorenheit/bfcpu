1bfc0f33c2042d8a9e7e662f49af0ca0ee91e970
jorenheit <jorenheit@gmail.com>
2023-11-04 13:40:07 +0100

    wrote a script to generate rom binaries and worked on control unit in logisim

--- src/rom/create_rom.m
+++ src/rom/create_rom.m
@@ -1,11 +1,91 @@
-function table = create_rom()
+function create_rom(filename_base)
   
-  nBits = 3;
-  table = zeros(2^nBits, 1);
+  % D  = 000 - none
+  % DP = 001 - RS0
+  % SP = 010 - RS1
+  % F  = 011 - RS0 | RS1
+  % I  = 100 - RS2
+  % IP = 101 - RS0 | RS2
+  % LS = 110 - RS1 | RS2
   
-  pattern = getPattern("PLUS", 2, "0x10");
-  bits = getBits("LD_D", "EN_D", "CR", "OE_RAM");
-  table = addInstruction(getPattern("PLUS", 1, "x01x"), getBits("INC(D)", "VE", "LD_F"));
+  table = {
+  
+  {"ANY",   0, "xxxx"}, {"EN_IP", "LD_I"};
+  
+  {"PLUS",  1, "x01x"}, {"INC", "VE", "LD_F"};
+  {"PLUS",  2, "x01x"}, {"INC", "RS0", "RS2", "CR"};
+  {"PLUS",  1, "x11x"}, {"EN_DP", "LD_D", "OE_RAM", "LD_F", "CR"};
+  {"PLUS",  1, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+  
+  {"MINUS", 1, "x01x"}, {"DEC", "VE", "LD_F"};
+  {"MINUS", 2, "x01x"}, {"INC", "RS0", "RS2", "CR"};
+  {"MINUS", 1, "x11x"}, {"EN_DP", "LD_D", "OE_RAM", "LD_F", "CR"}; 
+  {"MINUS", 1, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+  
+  {"RIGHT", 1, "0x1x"}, {"INC", "RS0", "AE", "LD_F"};
+  {"RIGHT", 2, "0x1x"}, {"INC", "RS0", "RS2", "CR"};
+  {"RIGHT", 1, "1x1x"}, {"EN_D", "EN_DP", "WE_RAM", "LD_F", "CR"};
+  {"RIGHT", 1, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+    
+  {"LEFT",  1, "0x1x"}, {"DEC", "RS0", "AE", "LD_F"};
+  {"LEFT",  2, "0x1x"}, {"INC", "RS0", "RS2", "CR"};
+  {"LEFT",  1, "1x1x"}, {"EN_D", "EN_DP", "WE_RAM", "LD_F", "CR"};
+  {"LEFT",  1, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+  
+  {"LOOP_START", 1, "x011"}, {"INC", "RS1", "RS2"};
+  {"LOOP_START", 2, "x011"}, {"INC", "RS0", "RS1", "CR"};
+  {"LOOP_START", 1, "x010"}, {"INC", "RS1"};
+  {"LOOP_START", 2, "x010"}, {"WE_RAM", "EN_SP", "EN_IP"};
+  {"LOOP_START", 3, "x010"}, {"INC", "RS0", "RS2", "CR"};
+  {"LOOP_START", 1, "x11x"}, {"EN_DP", "LD_D", "OE_RAM"};
+  {"LOOP_START", 2, "x11x"}, {"LD_F", "CR"};
+  {"LOOP_START", 1, "xx0x"}, {"INC", "RS1", "RS2"};
+  {"LOOP_START", 2, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+  
+  {"LOOP_END", 1, "x011"}, {"DEC", "RS1"};
+  {"LOOP_END", 2, "x011"}, {"INC", "RS0", "RS2", "CR"};
+  {"LOOP_END", 1, "x010"}, {"EN_SP", "OE_RAM", "LD_IP"};
+  {"LOOP_END", 2, "x010"}, {"INC", "RS0", "RS2", "CR"};
+  {"LOOP_END", 1, "x11x"}, {"EN_DP", "OE_RAM", "LD_D"};
+  {"LOOP_END", 2, "x11x"}, {"LD_F", "CR"};
+  {"LOOP_END", 1, "xx0x"}, {"DEC", "RS1", "RS2"};
+  {"LOOP_END", 2, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+  
+  {"OUT", 1, "x01x"}, {"PRE", "EN_D", "INC", "RS0", "RS2", "CR"};
+  {"OUT", 1, "x11x"}, {"EN_DP", "OE_RAM", "LD_D"}; 
+  {"OUT", 2, "x11x"}, {"PRE", "EN_D", "INC", "RS0", "RS2", "CR"};
+  {"OUT", 1, "xx0x"}, {"INC", "RS0", "RS2", "CR"};
+  
+  % TBD: input
+  };
+  
+ 
+  printf("Building Table ...\n");
+  
+  nBits = 11; % Number of address bits = 4 (cmd) + 4 (flags) + 3 (cycle)
+  result = NaN * ones(2^nBits, 1);
+  for i = 1:size(table, 1)
+    result = addInstruction(result, getPattern(table{i, 1}), getBits(table{i, 2}));
+  endfor
+  
+  % Fill remaining addresses with the ERR instruction
+  for i = 1:size(result, 1)
+    if isnan(result(i))
+      result(i) = getBits({"ERR", "CR"});
+    endif
+  endfor
+  
+  nRom = ceil((log(max(result))/log(2)) / 8);
+  for i = 1:nRom
+    filename = [filename_base, num2str(i), ".bin"];
+    printf("Writing to file %s\n", filename);
+    fid = fopen(filename, "w+b");
+    current = bitand(result, 0xff);
+    result = bitshift(result, -8);
+    
+    fwrite(fid, current, "uchar");
+    fclose(fid);
+  endfor
   
 endfunction
 
@@ -28,7 +108,11 @@ function table = addInstruction(table, pattern, value)
   % address is a match to the pattern.
   for addr = 0:N-1
     if isMatch(addr, pattern)
-      table(addr + 1) = value;
+      i = addr + 1;
+      if ~isnan(table(i))
+        error("Duplicate patterns")
+      endif
+      table(i) = value;
     endif
   endfor
 endfunction
@@ -54,7 +138,7 @@ function result = isMatch(address, pattern)
 endfunction
 
 
-function value = getBits(cmd, cycle, varargin)
+function value = getBits(args)
   
   signals = {
     "RS0",
@@ -72,20 +156,28 @@ function value = getBits(cmd, cycle, varargin)
     "LD_IP",
     "LD_I",
     "LD_F",
-    "CR"
+    "CR",
+    "VE",
+    "AE",
+    "PRE"
+    "ERR"
   };
   
   
   value = 0;
-  for i = 1:length(varargin)
-    sig = varargin{i};
+  for i = 1:length(args)
+    sig = args{i};
     idx = find(ismember(signals, sig)) - 1;
     value += 2^idx;
   endfor
   
 endfunction
 
-function pattern = getPattern(cmd, cycle, flags)
+function pattern = getPattern(state)
+  
+  cmd = state{1};
+  cycle = state{2};
+  flags = state{3};
   
   commands = {
     "NOOP",
@@ -97,8 +189,7 @@ function pattern = getPattern(cmd, cycle, flags)
     "OUT",
     "LOOP_START",
     "LOOP_END",
-    "HLT",
-    "ERR"
+    "HLT"
   };
   commandBits = 4;
   cycleBits = 3;
@@ -106,13 +197,9 @@ function pattern = getPattern(cmd, cycle, flags)
   if strcmp(cmd, "ANY")
     cmd = "xxxx";
   else
-    cmd = dec2bin(find(ismember(commands, cmd)));
-    cmd = [char('0' + zeros(1, commandBits - length(cmd))), cmd];
+    cmd = dec2bin(find(ismember(commands, cmd)) - 1, commandBits);
   endif
-  
-  cycle = dec2bin(cycle);
-  cycle = [char('0' + zeros(1, cycleBits - length(cycle))), cycle];
-  
+  cycle = dec2bin(cycle, cycleBits);
   pattern = [cmd, cycle, flags];
   
 endfunction  
