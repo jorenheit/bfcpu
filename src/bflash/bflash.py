5bc6b25b377e5d36158c7d2d18515d98d22b6b3c
jorenheit <jorenheit@gmail.com>
2023-11-18 16:37:35 +0100

    last commit had old version of bflash. updated

--- src/bflash/bflash.py
+++ src/bflash/bflash.py
@@ -3,6 +3,8 @@ import serial
 import time
 import threading
 import math
+import readline
+import os
 
 DUMP_REQUEST = b"\0"
 FLASH_REQUEST = b"\1"
@@ -11,10 +13,11 @@ PROGRAMMER_FINISHED = b"\3"
 
 ready = threading.Event()
 
-def show_percentage(current, total, previous):
+
+def show_percentage(current, total, previous, msg):
     new_percentage = math.floor(current / total * 10) * 10
     if new_percentage > previous:
-        print("{}%".format(new_percentage))
+        print(f"{msg} {new_percentage}%")
         return new_percentage
     return previous
 
@@ -39,27 +42,44 @@ def read_data(ser, file):
     ser.write(DUMP_REQUEST)
     data_length = int.from_bytes(ser.read(4), byteorder='little')
     data = bytearray()
-    print("Downloading data from ROM")
     percentage = 0
     for i in range(0, data_length):
-        percentage = show_percentage(i, data_length, percentage)
+        percentage = show_percentage(i, data_length, percentage, "Downloading... ")
         data += ser.read()
-    print("100%")
-
+    print("Downloading... 100%")
+        
     file.write(data)
 
 
 def serial_monitor(ser, keep_alive):
-    while True:
-        c = ser.read()
-        if c == PROGRAMMER_READY:
-            ready.set()
-            if not keep_alive:
-                return
-        elif c == PROGRAMMER_FINISHED:
-            break
-        else:
-            print(c.decode(), end="")
+    try:
+        while True:
+            c = ser.read()
+            if c == PROGRAMMER_READY:
+                ready.set()
+                if not keep_alive:
+                    return
+            elif c == PROGRAMMER_FINISHED:
+                break
+            else:
+                print(c.decode(), end="")
+    except Exception as e:
+        print(f"Error in serial_monitor: {e}")
+
+
+def input_with_autocomplete(prompt, completer):
+    readline.set_completer(completer)
+    readline.parse_and_bind('tab: complete')
+    try:
+        return input(prompt)
+    finally:
+        readline.set_completer()
+
+
+def filename_completer(text, state):
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    files = [f for f in files if f.startswith(text)]
+    return files[state] if state < len(files) else None
 
 
 class SerialHandler:
@@ -70,13 +90,15 @@ class SerialHandler:
         self.serial_monitor_keep_alive = serial_monitor_keep_alive
 
     def handle(self, args):
-        filename = input(self.prompt)
+        filename = input_with_autocomplete(self.prompt, filename_completer).strip()
         try:
             with serial.Serial(args.port, args.baudrate, timeout=None) as ser, \
                     open(filename, self.file_flags) as file:
 
-                writer = threading.Thread(target=self.worker, args=(ser, file))
-                monitor = threading.Thread(target=serial_monitor, args=(ser, self.serial_monitor_keep_alive))
+                writer = threading.Thread(target=self.worker, \
+                                          args=(ser, file))
+                monitor = threading.Thread(target=serial_monitor, \
+                                           args=(ser, self.serial_monitor_keep_alive))
 
                 writer.start()
                 monitor.start()
@@ -92,7 +114,8 @@ class SerialHandler:
 
 def prompt():
     while True:
-        choice = input("Enter (D) to dump the current ROM to a file or (F) to flash a file onto the ROM: ").lower()
+        choice = input_with_autocomplete("Enter (D) to dump the current ROM to a file or (F) to flash a file onto the ROM: ", \
+                                         filename_completer).lower()
         if choice in ["d", "f"]:
             return choice
         else:
@@ -105,18 +128,12 @@ def main():
     parser.add_argument('baudrate', type=int, help='Baud rate for the serial connection')
 
     args = parser.parse_args()
-    
-    dump_policy = SerialHandler(read_data, "wb", "Enter the file to dump ROM into: ", False)
-    flash_policy = SerialHandler(write_data, "rb", "Enter the file to flash onto ROM: ", True)
-    
-    while True:
-        choice = prompt()
-        if choice == "d":
-            dump_policy.handle(args)
-            break
-        elif choice == "f":
-            flash_policy.handle(args)
-            break
+
+    choice = prompt()
+    if choice == "d":
+        SerialHandler(read_data, "wb", "Enter the file to dump ROM into: ", False).handle(args)
+    elif choice == "f":
+        SerialHandler(write_data, "rb", "Enter the file to flash onto ROM: ", True).handle(args)
 
 
 if __name__ == "__main__":
